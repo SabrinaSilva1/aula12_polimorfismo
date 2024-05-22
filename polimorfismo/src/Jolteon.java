526513f4a7ffe3e7781dd3de5daff01403c56690
Sabrina <sabrinarochasilva225@gmail.com>
2024-05-22 12:34:18 -0300

    Iniciando o polimorfismo

---
+++ polimorfismo/src/Jolteon.java
@@ -0,0 +1,17 @@
+public class Jolteon extends Eevee {
+
+    @Override
+    public String ataque(){
+        return "Trovoada - Thunder-Shock";
+    }
+
+    @Override
+    public String defesa(){
+        return "Larga - Wild Charge";
+    }
+
+    @Override
+    public String especial(){
+        return "Raio - Thunder";
+    }
+}
